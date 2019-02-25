04e8629391cc4d1e3c106e601da7cd3b9a6ac42c
Saso Kiselkov <skiselkov@gmail.com>
2019-02-25 14:01:44 +0100

    dem_stitch should use multi-threading during DSF extraction.

--- dem_stitch.sh
+++ dem_stitch.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# CDDL HEADER START
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+# CDDL HEADER END
+#
+# Copyright 2019 Saso Kiselkov. All rights reserved.
+#
 
 HERE="$(dirname "$0")"
 
@@ -7,8 +23,19 @@ CONVERT="convert"
 EXTRACT="$HERE/dsf_dem_extract"
 RESOLUTION=1024x1024
 REMOVE_TEMP=0
+NCPUS=$(grep '^processor\>' /proc/cpuinfo | wc -l)
 
 set -e
+
+# Executes up to NCPUS of parallel job tasks using the
+# arguments of this function as the command (+args) to run.
+function taskq ()
+{
+	while (( $(jobs | wc -l) > $NCPUS )); do
+		wait -n
+	done
+	"$@" &
+}
 
 while getopts "r:" o; do
 	case "${o}" in
@@ -40,9 +67,10 @@ for DSFFILE in "$INDIR"/*.dsf; do
 	PNGFILE="${PNGFILE/%.dsf/.png}"
 	if ! [ -f "$TMPDIR/$PNGFILE" ]; then
 		echo "[EXTRACT]  $(basename "$DSFFILE")"
-		"$EXTRACT" -o "$DSFFILE" "$TMPDIR/$PNGFILE"
+		taskq "$EXTRACT" -o "$DSFFILE" "$TMPDIR/$PNGFILE"
 	fi
 done
+wait
 
 # Fill in empty slots
 BASE_X="$(echo ${BASEDIR:3:4} | sed 's/^\([+-]\)0\+\([1-9]\)/\1\2/g')"
